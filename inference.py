20c9f6e1215fa67756d7446660b28335f3624ff3
rickyHong <jh74.hong@samsung.com>
2023-07-07 14:02:28 +0900

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,40 @@
+import torch
+from peft import PeftModel
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+#model_name = "huggyllama/llama-7b"
+#adapters_name = 'timdettmers/guanaco-7b'
+model_name = "EleutherAI/polyglot-ko-1.3b"
+adapters_name = 'output/output13b/adapter_model'
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    load_in_4bit=True,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+    max_memory= {i: '24000MB' for i in range(torch.cuda.device_count())},
+    quantization_config=BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type='nf4'
+    ),
+)
+model = PeftModel.from_pretrained(
+        model,
+        adapters_name,
+        #local_files_only=True
+        )
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+#prompt = "Introduce yourself"
+prompt = "안녕"
+formatted_prompt = (
+    f"A chat between a curious human and an artificial intelligence assistant."
+    f"The assistant gives helpful, detailed, and polite answers to the user's questions.\n"
+    f"### Human: {prompt} ### Assistant:"
+)
+inputs = tokenizer(formatted_prompt, return_tensors="pt").to("cuda:0")
+outputs = model.generate(inputs=inputs.input_ids, max_new_tokens=20)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
